04bb936aa3bc28a3947f041707c0050eae19c9c3
tal-hash1 <tal.zemel@horizon3.ai>
2025-10-03 01:20:43 +0200

    Update index.jsx

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './pages/App.jsx';
+
 
 createRoot(document.getElementById('root')).render(<App />)
